6d7c2a5686b17e89f1613f4ade23b301a481e052
Carolina Helena da Silva Corrêa <carolcorrea555@gmail.com>
2026-05-07 22:58:25 -0300

    Removing commented out code

--- jab-rpa-server/src/jab_wrapper.rs
+++ jab-rpa-server/src/jab_wrapper.rs
@@ -1,5 +1,5 @@
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{Arc, Mutex, Weak};
+use std::sync::{Arc, Mutex};
 use tokio::sync::mpsc;
 
 use crate::bindings;
@@ -24,18 +24,12 @@ impl Drop for JavaObject {
     }
 }
 
-// Global weak reference to JabWrapper for callbacks
-static mut JAB_WRAPPER: *mut Weak<JabWrapper> = std::ptr::null_mut();
-
 pub struct JabWrapper {
     pub initialized: AtomicBool,
     message_pump_handle: Mutex<Option<std::thread::JoinHandle<()>>>,
     message_pump_thread_id: Mutex<Option<u32>>,
     event_tx: Mutex<Option<mpsc::Sender<crate::JabCallbackEvent>>>,
 }
-
-unsafe impl Send for JabWrapper {}
-unsafe impl Sync for JabWrapper {}
 
 impl JabWrapper {
     pub fn new() -> Arc<Self> {
@@ -45,11 +39,6 @@ impl JabWrapper {
             message_pump_thread_id: Mutex::new(None),
             event_tx: Mutex::new(None),
         });
-
-        // Set global weak reference to wrapper
-        unsafe {
-            JAB_WRAPPER = Box::into_raw(Box::new(Arc::downgrade(&wrapper))) as *mut _;
-        }
 
         // Channel to synchronize initialization
         let (init_tx, init_rx) = std::sync::mpsc::channel::<bool>();
@@ -92,33 +81,6 @@ impl JabWrapper {
 
         wrapper
     }
-
-    // fn register_callbacks(&self) {
-    //     unsafe {
-    //         bindings::SetFocusGained(Some(focus_gained_cb));
-    //         bindings::SetFocusLost(Some(focus_lost_cb));
-    //         bindings::SetCaretUpdate(Some(caret_update_cb));
-    //         bindings::SetMouseClicked(Some(mouse_clicked_cb));
-    //         bindings::SetMouseEntered(Some(mouse_entered_cb));
-    //         bindings::SetMouseExited(Some(mouse_exited_cb));
-    //         bindings::SetMousePressed(Some(mouse_pressed_cb));
-    //         bindings::SetMouseReleased(Some(mouse_released_cb));
-    //         bindings::SetPropertyNameChange(Some(property_name_change_cb));
-    //         bindings::SetPropertyDescriptionChange(Some(property_description_change_cb));
-    //         bindings::SetPropertyStateChange(Some(property_state_change_cb));
-    //         bindings::SetPropertyValueChange(Some(property_value_change_cb));
-    //         bindings::SetPropertySelectionChange(Some(property_selection_change_cb));
-    //         bindings::SetPropertyTextChange(Some(property_text_change_cb));
-    //         bindings::SetPropertyCaretChange(Some(property_caret_change_cb));
-    //         bindings::SetPropertyVisibleDataChange(Some(property_visible_data_change_cb));
-    //         bindings::SetPropertyChildChange(Some(property_child_change_cb));
-    //         bindings::SetPropertyActiveDescendentChange(Some(
-    //             property_active_descendent_change_cb,
-    //         ));
-    //         bindings::SetPropertyTableModelChange(Some(property_table_model_change_cb));
-    //         bindings::SetJavaShutdown(Some(java_shutdown_cb));
-    //     }
-    // }
 
     pub fn set_event_sender(&self, tx: mpsc::Sender<crate::JabCallbackEvent>) {
         let mut event_tx = self.event_tx.lock().unwrap();
@@ -210,7 +172,7 @@ impl JabWrapper {
             }
 
             for i in 0..actions.actionsCount {
-                let action_name = String::from_utf16_lossy(&actions.actionInfo[i as usize].name);
+                let action_name = utf16_to_string(&actions.actionInfo[i as usize].name);
                 if action_name.to_lowercase().contains("click") {
                     let mut actions_to_do: bindings::AccessibleActionsToDo = std::mem::zeroed();
                     actions_to_do.actionsCount = 1;
@@ -334,12 +296,12 @@ impl Drop for JabWrapper {
         }
 
         // Clean up global weak reference
-        unsafe {
-            if !JAB_WRAPPER.is_null() {
-                let _ = Box::from_raw(JAB_WRAPPER);
-                JAB_WRAPPER = std::ptr::null_mut();
-            }
-        }
+        // unsafe {
+        //     if !JAB_WRAPPER.is_null() {
+        //         let _ = Box::from_raw(JAB_WRAPPER);
+        //         JAB_WRAPPER = std::ptr::null_mut();
+        //     }
+        // }
 
         // Call shutdownAccessBridge after the message pump has exited
         unsafe {
@@ -347,28 +309,6 @@ impl Drop for JabWrapper {
         }
     }
 }
-
-// Standalone function to send callback events via the global weak reference
-// fn send_callback_event(mut event: crate::JabCallbackEvent) {
-//     unsafe {
-//         if !JAB_WRAPPER.is_null()
-//             && let Some(wrapper) = (*JAB_WRAPPER).upgrade()
-//         {
-//             // Try to convert the context_handle (JObject) to a proper handle
-//             let context = event.context_handle as JObject;
-//             let handle_map = wrapper.jobject_to_handle.lock().unwrap();
-//             if let Some(handle) = handle_map.get(&context) {
-//                 event.context_handle = *handle;
-//             }
-//
-//             // Send the event
-//             let event_tx = wrapper.event_tx.lock().unwrap();
-//             if let Some(tx) = &*event_tx {
-//                 let _ = tx.try_send(event);
-//             }
-//         }
-//     }
-// }
 
 fn run_message_pump() {
     unsafe {
@@ -389,260 +329,3 @@ fn run_message_pump() {
         }
     }
 }
-
-// extern "C" fn focus_gained_cb(vm_id: i32, _event: i64, source: i64) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "FocusGained".to_string(),
-//         vm_id,
-//         context_handle: source as u64, // Will be converted to handle in send_callback_event
-//         message: format!("source={}", source),
-//         event_time: 0, // TODO: get actual time
-//     });
-// }
-//
-// extern "C" fn focus_lost_cb(vm_id: i32, _event: i64, source: i64) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "FocusLost".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn caret_update_cb(vm_id: i32, _event: i64, source: i64) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "CaretUpdate".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn mouse_clicked_cb(vm_id: i32, _event: i64, source: i64) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "MouseClicked".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn mouse_entered_cb(vm_id: i32, _event: i64, source: i64) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "MouseEntered".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn mouse_exited_cb(vm_id: i32, _event: i64, source: i64) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "MouseExited".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn mouse_pressed_cb(vm_id: i32, _event: i64, source: i64) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "MousePressed".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn mouse_released_cb(vm_id: i32, _event: i64, source: i64) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "MouseReleased".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn property_name_change_cb(
-//     vm_id: i32,
-//     _event: i64,
-//     source: i64,
-//     _old_name: *mut u16,
-//     _new_name: *mut u16,
-// ) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "PropertyNameChange".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn property_description_change_cb(
-//     vm_id: i32,
-//     _event: i64,
-//     source: i64,
-//     _old_desc: *mut u16,
-//     _new_desc: *mut u16,
-// ) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "PropertyDescriptionChange".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn property_state_change_cb(
-//     vm_id: i32,
-//     _event: i64,
-//     source: i64,
-//     _old_state: *mut u16,
-//     _new_state: *mut u16,
-// ) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "PropertyStateChange".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn property_value_change_cb(
-//     vm_id: i32,
-//     _event: i64,
-//     source: i64,
-//     _old_value: *mut u16,
-//     _new_value: *mut u16,
-// ) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "PropertyValueChange".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn property_selection_change_cb(vm_id: i32, _event: i64, source: i64) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "PropertySelectionChange".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn property_text_change_cb(vm_id: i32, _event: i64, source: i64) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "PropertyTextChange".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn property_caret_change_cb(
-//     vm_id: i32,
-//     _event: i64,
-//     source: i64,
-//     old_pos: i32,
-//     new_pos: i32,
-// ) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "PropertyCaretChange".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!(
-//             "source={}, old_pos={}, new_pos={}",
-//             source, old_pos, new_pos
-//         ),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn property_visible_data_change_cb(vm_id: i32, _event: i64, source: i64) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "PropertyVisibleDataChange".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn property_child_change_cb(
-//     vm_id: i32,
-//     _event: i64,
-//     source: i64,
-//     old_child: i64,
-//     new_child: i64,
-// ) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "PropertyChildChange".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!(
-//             "source={}, old_child={}, new_child={}",
-//             source, old_child, new_child
-//         ),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn property_active_descendent_change_cb(
-//     vm_id: i32,
-//     _event: i64,
-//     source: i64,
-//     old_active: i64,
-//     new_active: i64,
-// ) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "PropertyActiveDescendentChange".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!(
-//             "source={}, old_active={}, new_active={}",
-//             source, old_active, new_active
-//         ),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn property_table_model_change_cb(
-//     vm_id: i32,
-//     _event: i64,
-//     source: i64,
-//     _old_model: *mut u16,
-//     _new_model: *mut u16,
-// ) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "PropertyTableModelChange".to_string(),
-//         vm_id,
-//         context_handle: source as u64,
-//         message: format!("source={}", source),
-//         event_time: 0,
-//     });
-// }
-//
-// extern "C" fn java_shutdown_cb(vm_id: i32) {
-//     send_callback_event(crate::JabCallbackEvent {
-//         event_type: "JavaShutdown".to_string(),
-//         vm_id,
-//         context_handle: 0,
-//         message: format!("vm_id={}", vm_id),
-//         event_time: 0,
-//     });
-// }
