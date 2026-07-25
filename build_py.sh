dc825e0f419a879716ef78e132cfd6fcd16b3314
Carolina Helena da Silva Corrêa <carolcorrea555@gmail.com>
2026-07-25 11:46:50 -0300

    Now bin package versions update dynamically on build_py.sh

--- build_py.sh
+++ build_py.sh
@@ -8,15 +8,24 @@ uv run python -m grpc_tools.protoc \
     --python_betterproto2_out=python/jab_rpa/proto \
     proto/jab.proto
 
-# 2. Build core package (no binaries)
+# 2. Update optional dependencies version
+VERSION=$(grep -Po '^version = "\K[^"]+' Cargo.toml)
+
+for java in 8 11 17 21 25; do
+    sed -Ei \
+        "s|(java${java}\s*=\s*\[\"jab-rpa-bin-java${java})[^\"]*(\"\])|\1==${VERSION}\2|" \
+        pyproject.toml
+done
+
+# 3. Build core package (no binaries)
 rm -f dist/*
 uv build
 
-# 3. Build binary packages
+# 4. Build binary packages
 for java_ver in "8" "11" "17" "21" "25"; do
     pkg="jab-rpa-bin-java$java_ver"
     uv build --package $pkg
 done
 
-# 4. Build docs
+# 5. Build docs
 uv run mkdocs build
